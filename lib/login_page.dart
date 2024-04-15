9bb15150fd4cc6e2e4ae29a6aba87bae638085aa
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2024-04-15 16:38:05 +0530

    second

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:backimglogin/pallete.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,6 +9,85 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        BackgroungImage(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  height: 150,
+                  child: Center(
+                    child: Text(
+                      'Foodybite',
+                      style: kHeading,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[600]?.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(vertical: 20),
+                            border: InputBorder.none,
+                            hintText: ' Email',
+                            prefixIcon: Icon(Icons.email, color: Colors.white,size: 30,),
+                            hintStyle: kBodyText,
+                          ),
+                          style: kBodyText,
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[600]?.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(vertical: 20),
+                            border: InputBorder.none,
+                            hintText: ' Email',
+                            prefixIcon: Icon(Icons.email, color: Colors.white,size: 30,),
+                            hintStyle: kBodyText,
+                          ),
+                          style: kBodyText,
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class BackgroungImage extends StatelessWidget {
+  const BackgroungImage({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
     return ShaderMask(
@@ -21,7 +101,6 @@ class _LoginPageState extends State<LoginPage> {
           image: DecorationImage(
             image: AssetImage('assets/images/new.png'),
             fit: BoxFit.cover,
-
           ),
         ),
       ),
